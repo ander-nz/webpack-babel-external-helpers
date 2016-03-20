3e9414117e84915fea07cdc31dead6f024838f07
Shane S. Anderson <s@ander.nz>
2016-03-21 09:22:30 +1300

    Adjusted webpack tester to clone options, since webpack mutates options.

--- test/support/webpack-helpers.js
+++ test/support/webpack-helpers.js
@@ -1,6 +1,9 @@
+const clone = require("clone")
 const webpack = require("webpack")
 
 exports.as_promise = function(options) {
+  options = clone(options)
+  
   return new Promise(function(resolve, reject) {
     webpack(options, function(err, stats) {
       if (err)
