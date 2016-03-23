c9484103ad549ed62e39219dce67d7563d73b591
Shane S. Anderon <s@ander.nz>
2016-03-23 13:15:36 +1300

    Refactored tmp folder helpers

--- test/support/helpers.js
+++ test/support/helpers.js
@@ -1,4 +1,4 @@
-const tmp = require("./tmp-helpers")
+const tmp = require("./tmp-folder")
 const webpackAsPromised = require("./webpack-as-promised")
 const BabelExternalHelpers = require("../../src/index.js")
 
@@ -9,26 +9,26 @@ const BABEL_LOADER = {
 }
 
 const WEBPACK_PLUGINLESS = {
-  entry: [tmp.INPUT_PATH],
-  output: { filename: tmp.OUTPUT_PATH },
+  entry: [tmp.inputjs.path],
+  output: { filename: tmp.outputjs.path },
   module: { loaders: [BABEL_LOADER] },
 }
 
 const WEBPACK_WITHPLUGIN = {
-  entry: [tmp.INPUT_PATH],
-  output: { filename: tmp.OUTPUT_PATH },
+  entry: [tmp.inputjs.path],
+  output: { filename: tmp.outputjs.path },
   module: { loaders: [BABEL_LOADER] },
   plugins: [new BabelExternalHelpers("*")],
 }
 
 global.webpack_pluginless = function(srcInput) {
   tmp.cleanup()
-  tmp.writeInputSync(srcInput)
-  return webpackAsPromised(WEBPACK_PLUGINLESS).then(tmp.readOutputSync)
+  tmp.inputjs.write(srcInput)
+  return webpackAsPromised(WEBPACK_PLUGINLESS).then(tmp.outputjs.read)
 }
 
 global.webpack_with_plugin = function(srcInput) {
   tmp.cleanup()
-  tmp.writeInputSync(srcInput)
-  return webpackAsPromised(WEBPACK_WITHPLUGIN).then(tmp.readOutputSync)
+  tmp.inputjs.write(srcInput)
+  return webpackAsPromised(WEBPACK_WITHPLUGIN).then(tmp.outputjs.read)
 }
