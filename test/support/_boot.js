75a44aad85e87f149e3776913fff5c5e5d9c709b
Shane S. Anderon <s@ander.nz>
2016-03-23 16:21:37 +1300

    Rearranged support boot file and options.

--- test/support/_boot.js
+++ test/support/_boot.js
@@ -1,4 +1,8 @@
+// setup unit-testing framework
 const chai = require("chai")
 const chaiAsPromised = require("chai-as-promised")
 chai.should()
 chai.use(chaiAsPromised)
+
+// setup other helpers
+require("./helpers")
